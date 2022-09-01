048136ba503e9476cb00db63b12202cfd8de69cd
jun <junpeeei0407@gmail.com>
2022-09-01 18:50:46 +0900

    react基礎

--- demo_react/src/App.jsx
+++ demo_react/src/App.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import { useEffect } from 'react';
 import { useState } from 'react';
 import ColorFulMessage from './components/ColorfulMessage';
 
 const App = () => {
+
+    console.log("最初");
 
     const [num, setNum] = useState(0);
     const [faceShowFlag, setFaceShowFlag] = useState(false);
@@ -15,13 +18,17 @@ const App = () => {
         setFaceShowFlag(!faceShowFlag);
     };
 
-    if (num > 0) {
-        if (num % 3 === 0) {
-            faceShowFlag || setFaceShowFlag(true);
-        } else {
-            faceShowFlag && setFaceShowFlag(false);
+    useEffect(() => {
+        if (num > 0) {
+            if (num % 3 === 0) {
+                faceShowFlag || setFaceShowFlag(true);
+            } else {
+                faceShowFlag && setFaceShowFlag(false);
+            }
         }
-    }
+    // eslint-disable-next-line 
+    },[num]);
+    
 
 
     return (
--- demo_react/src/components/ColorfulMessage.jsx
+++ demo_react/src/components/ColorfulMessage.jsx
@@ -2,9 +2,10 @@ import React from 'react';
 
 const ColorFulMessage = ({color, text}) => {
 
+
     return(
         <p>{text}</p>
     );
 }
 
-export default ColorFulMessage;
+export default ColorFulMessage
